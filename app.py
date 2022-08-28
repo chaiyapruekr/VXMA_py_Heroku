0fe3e34b8c96b509aa7b8989f01f8fa91293c024
Frozatquadtine <the4wonz@outlook.com>
2022-08-28 07:13:17 +0700

    1.0

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return 'VXMA bot (Form Tradingview)By Vaz. \nDonate XMR : 87tT3DZqi4mhGuJjEp3Yebi1Wa13Ne6J7RGi9QxU21FkcGGNtFHkfdyLjaPLRv8T2CMrz264iPYQ2dCsJs2MGJ27GnoJFbm'
+    return BOT_NAME + 'By Vaz. \nDonate XMR : 87tT3DZqi4mhGuJjEp3Yebi1Wa13Ne6J7RGi9QxU21FkcGGNtFHkfdyLjaPLRv8T2CMrz264iPYQ2dCsJs2MGJ27GnoJFbm'
 
 #key setting str(os.environ['API_KEY'])
 API_KEY = str(os.environ['API_KEY'])
@@ -78,91 +78,86 @@ if MIN_BALANCE[0]=='$':
 #notify.send(wellcome)
 
 #Alphatrend
-def alphatrend(df, period=int(ATR_Period), atr_multiplier=float(ATR_Mutiply)):
-    df['atr'] = ta.atr(df['high'],df['low'],df['close'], period)
-    df['rsi'] = ta.rsi(df['close'],int(RSI_Period))
-    df['downT'] = df['high'] + (atr_multiplier * df['atr'])
-    df['upT'] = df['low'] - (atr_multiplier * df['atr'])
+def alphatrend(df,atr_p,atr_m,rsi):
+    df['atr'] = ta.sma(ta.true_range(df['High'],df['Low'],df['Close']),atr_p)
+    df['rsi'] = ta.rsi(df['Close'],rsi)
+    df['downT'] = 0.0
+    df['upT'] = 0.0
     df['alphatrend'] = 0.0
+#AlphaTrend rsibb >= 50 ? upT < nz(AlphaTrend[1]) ? nz(AlphaTrend[1]) : upT : downT > nz(AlphaTrend[1]) ? nz(AlphaTrend[1]) : downT
     for current in range(1, len(df.index)):
         previous = current - 1
+        df['downT'][current] = df['High'][current] + df['atr'][current] * atr_m
+        df['upT'][current] = df['Low'][current] - df['atr'][current] * atr_m
         if df['rsi'][current] >= 50 :
-            if df['upT'][current] < df['alphatrend'][previous]:
-                df['alphatrend'][current] = df['alphatrend'][previous]
+            if df['upT'][current] < (df['alphatrend'][previous] if df['alphatrend'][previous] != None else 0):
+                df['alphatrend'][current] = (df['alphatrend'][previous] if df['alphatrend'][previous] != None else 0)
             else : df['alphatrend'][current] = df['upT'][current]
         else:
-            if df['downT'][current] > df['alphatrend'][previous]:
-                df['alphatrend'][current] = df['alphatrend'][previous]
+            if df['downT'][current] > (df['alphatrend'][previous] if df['alphatrend'][previous] != None else 0):
+                df['alphatrend'][current] = (df['alphatrend'][previous] if df['alphatrend'][previous] != None else 0)
             else : df['alphatrend'][current] = df['downT'][current]
-        
     return df
+    
 #Andean_Oscillator
-def andean(df):
-    df['cmpbull'] = 0.0
-    df['cmpbear'] = 0.0
+def andean(df,AOL):
     df['up1'] = 0.0
     df['up2'] = 0.0
     df['dn1'] = 0.0
     df['dn2'] = 0.0
-    alpha = 2/(int(LengthAO) + 1)
+    df['cmpbull'] = 0.0
+    df['cmpbear'] = 0.0
+    alpha = 2/(AOL + 1)
     for current in range(1, len(df.index)):
         previous = current - 1
-        CloseP = df['close'][current]
-        OpenP = df['open'][current]
+        CloseP = df['Close'][current]
+        OpenP = df['Open'][current]
         up1 = df['up1'][previous]
         up2 = df['up2'][previous]
         dn1 = df['dn1'][previous]
         dn2 = df['dn2'][previous]
         # up1 := nz(math.max(C, O, up1[1] - (up1[1] - C) * alpha), C)
-        if max(CloseP,OpenP,up1 - (up1 - CloseP)*alpha) == 0:
-            df['up1'][current] = df['close'][current]
-        else : 
-            df['up1'][current] = max(CloseP,OpenP,up1 - (up1 - CloseP)*alpha)
+        df['up1'][current] = (max(CloseP,OpenP,up1 - (up1 - CloseP)*alpha) if max(CloseP,OpenP,up1 - (up1 - CloseP)*alpha) != None else df['Close'][current])
         # up2 := nz(math.max(C * C, O * O, up2[1] - (up2[1] - C * C) * alpha), C * C)
-        if max(CloseP*CloseP,OpenP*OpenP,up2 - (up2 - CloseP*CloseP)*alpha) == 0:
-            df['up2'][current] = df['close'][current]*df['close'][current]
-        else :
-            df['up2'][current] = max(CloseP*CloseP,OpenP*OpenP,up2 - (up2 - CloseP*CloseP)*alpha)
+        df['up2'][current] = (max(CloseP*CloseP,OpenP*OpenP,up2 - (up2 - CloseP*CloseP)*alpha) if max(CloseP*CloseP,OpenP*OpenP,up2 - (up2 - CloseP*CloseP)*alpha) != None else df['Close'][current]*df['Close'][current])
         # dn1 := nz(math.min(C, O, dn1[1] + (C - dn1[1]) * alpha), C)
-        if min(CloseP,OpenP,dn1 + (CloseP - dn1)*alpha) == 0:
-            df['dn1'][current] = df['close'][current]
-        else :
-            df['dn1'][current] = min(CloseP,OpenP,dn1 + (CloseP - dn1)*alpha)
+        df['dn1'][current] = (min(CloseP,OpenP,dn1 + (CloseP - dn1)*alpha) if min(CloseP,OpenP,dn1 + (CloseP - dn1)*alpha) != None else df['Close'][current])
         # dn2 := nz(math.min(C * C, O * O, dn2[1] + (C * C - dn2[1]) * alpha), C * C)
-        if min(CloseP*CloseP,OpenP*OpenP,dn2 + (CloseP*CloseP - dn2)*alpha) ==0:
-            df['dn2'][current] = df['close'][current]*df['close'][current]
-        else :
-            df['dn2'][current] = min(CloseP*CloseP,OpenP*OpenP,dn2 + (CloseP*CloseP - dn2)*alpha)
-        up1n = df['up1'][current]
+        df['dn2'][current] = (min(CloseP*CloseP,OpenP*OpenP,dn2 + (CloseP*CloseP - dn2)*alpha) if min(CloseP*CloseP,OpenP*OpenP,dn2 + (CloseP*CloseP - dn2)*alpha) != None else df['Close'][current]*df['Close'][current])
+        up1n = df['up1'][current] 
         up2n = df['up2'][current]
         dn1n = df['dn1'][current]
         dn2n = df['dn2'][current]
         df['cmpbull'][current] = math.sqrt(dn2n - (dn1n * dn1n))
         df['cmpbear'][current] = math.sqrt(up2n - (up1n * up1n))
     return df
+    
 #VXMA Indicator
 def vxma(df):
     df['vxma'] = 0.0
     df['trend'] = False
+    df['buy'] = False
+    df['sell'] = False
     for current in range(2, len(df.index)):
         previous = current - 1
         before  = current - 2
         EMAFAST = df['ema'][current]
         LINREG = df['subhag'][current]
         ALPHATREND = df['alphatrend'][before]
-        clohi = min(EMAFAST,LINREG,ALPHATREND)
-        clolo = max(EMAFAST,LINREG,ALPHATREND)
-#CloudMA := (bull > bear) ? clolo < nz(CloudMA[1]) ? nz(CloudMA[1]) : clolo : (bear > bull) ? clohi > nz(CloudMA[1]) ? nz(CloudMA[1]) : clohi : nz(CloudMA[1])
+        clohi = max(EMAFAST,LINREG,ALPHATREND)
+        clolo = min(EMAFAST,LINREG,ALPHATREND)
+#CloudMA := (bull > bear) ? clolo < nz(CloudMA[1]) ? nz(CloudMA[1]) : clolo :
         if df['cmpbull'][current] > df['cmpbear'][current] :
-            if clolo < df['vxma'][previous]:
-                df['vxma'][current] = df['vxma'][previous]
+            if clolo < (df['vxma'][previous] if df['vxma'][previous] != None else 0):
+                df['vxma'][current] = (df['vxma'][previous] if df['vxma'][previous] != None else 0)
             else : df['vxma'][current] = clolo
+#  (bear > bull) ? clohi > nz(CloudMA[1]) ? nz(CloudMA[1]) : clohi : nz(CloudMA[1])
         elif df['cmpbull'][current] < df['cmpbear'][current]:
-            if clohi > df['vxma'][previous]:
-                df['vxma'][current] = df['vxma'][previous]
+            if clohi > (df['vxma'][previous] if df['vxma'][previous] != None else 0):
+                df['vxma'][current] = (df['vxma'][previous] if df['vxma'][previous] != None else 0)
             else : df['vxma'][current] = clohi
         else:
-            df['vxma'][current] = df['vxma'][previous]
+            df['vxma'][current] = (df['vxma'][previous] if df['vxma'][previous] != None else 0)
         #Get trend True = Bull False = Bear
         if df['vxma'][current] > df['vxma'][previous] and df['vxma'][previous] > df['vxma'][before] :
             df['trend'][current] = True
@@ -170,21 +165,32 @@ def vxma(df):
             df['trend'][current] = False
         else:
             df['trend'][current] = df['trend'][previous] 
-   
+        #get zone
+        if df['trend'][current] and not df['trend'][previous] :
+            df['buy'][current] = True
+            df['sell'][current] = False
+        elif not df['trend'][current] and df['trend'][previous] :
+            df['buy'][current] = False
+            df['sell'][current] = True
+        else:
+            df['buy'][current] = False
+            df['sell'][current] = False
     return df
+
 #Pivot High-Low
 def pivot(df):
-    df['highest'] = df['high']
-    df['lowest'] = df['low']
+    df['Highest'] = df['High']
+    df['Lowest'] = df['Low']
     for current in range(len(df.index) - int(Pivot), len(df.index)):
         previous = current - 1
-        if df['low'][current] < df['lowest'][previous]:
-            df['lowest'][current] = df['low'][current]
-        else : df['lowest'][current] = df['lowest'][previous]
-        if df['high'][current] > df['highest'][previous]:
-            df['highest'][current] = df['high'][current]
-        else : df['highest'][current] = df['highest'][previous]
+        if df['Low'][current] < df['Lowest'][previous]:
+            df['Lowest'][current] = df['Low'][current]
+        else : df['Lowest'][current] = df['Lowest'][previous]
+        if df['High'][current] > df['Highest'][previous]:
+            df['Highest'][current] = df['High'][current]
+        else : df['Highest'][current] = df['Highest'][previous]
     return df
+
 #Position Sizing
 def buysize(df,balance,symbol):
     last = len(df.index) - 1
@@ -195,10 +201,11 @@ def buysize(df,balance,symbol):
         risk = (percent/100)*freeusd
     elif RISK[0]=='$':
         risk = float(RISK[1:len(RISK)])
-    amount = abs(risk  / (df['close'][last] - df['lowest'][last]))
+    amount = abs(risk  / (df['Close'][last] - df['Lowest'][last]))
     qty_precision = exchange.markets[symbol]['precision']['amount']
     lot = round(amount,qty_precision)
     return lot
+
 def sellsize(df,balance,symbol):
     last = len(df.index) - 1
     exchange.load_markets()
@@ -208,23 +215,25 @@ def sellsize(df,balance,symbol):
         risk = (percent/100)*freeusd
     elif RISK[0]=='$':
         risk = float(RISK[1:len(RISK)])
-    amount = abs(risk  / (df['highest'][last] - df['close'][last]))
+    amount = abs(risk  / (df['Highest'][last] - df['Close'][last]))
     qty_precision = exchange.markets[symbol]['precision']['amount']
     lot = round(amount,qty_precision)
     return lot
-    
+
+#RiskReward    
 def RRTP(df,symbol,direction):
-    #buyprice  * (1 + (((openprice - lowest)/openprice))*rrPer) 
-    #sellprice * (1 - (((highest - openprice)/ openprice))*rrPer)
+    #buyprice  * (1 + (((Openprice - Lowest)/Openprice))*rrPer) 
+    #sellprice * (1 - (((Highest - Openprice)/ Openprice))*rrPer)
     # true = long, false = short
     if direction :
-        ask = exchange.fetchBidsAsks([symbol])[symbol]['info']['askPrice']
-        target = ask *(1+((ask-df['lowest'][len(df.index)-1])/ask)*TPRR1)
+        ask = exchange.fetchBidsAsks([symboli])[symboli]['info']['askPrice']
+        target = ask *(1+((ask-df['Lowest'][len(df.index)-1])/ask)*TPRR1)
     else :
-        bid = exchange.fetchBidsAsks([symbol])[symbol]['info']['bidPrice']
-        target = bid *(1-((df['highest'][len(df.index)-1]-bid)/bid)*TPRR1)
+        bid = exchange.fetchBidsAsks([symboli])[symboli]['info']['bidPrice']
+        target = bid *(1-((df['Highest'][len(df.index)-1]-bid)/bid)*TPRR1)
     return target
 
+#OpenLong Buy
 def OpenLong(df,balance,symbol,lev):
     print('Entry Long')
     amount = float(buysize(df,balance,symbol))
@@ -232,12 +241,12 @@ def OpenLong(df,balance,symbol,lev):
     params = {
     'stopLoss': {
         'type': 'market', # or 'limit'
-        'stopLossPrice': df['lowest'][len(df.index)-1],
+        'stopLossPrice': str(df['Lowest'][len(df.index)-1]),
     },
     'takeProfit': {
         'type': 'market',
-        'takeProfitPrice': RRTP(df,symbol,True),
-        'quantity': (amount*int(TPPer)/100),
+        'takeProfitPrice': str(RRTP(df,symbol,True)),
+        'quantity': str(amount*int(TPPer)/100),
     },
     'positionSide': Lside 
     }
@@ -252,7 +261,8 @@ def OpenLong(df,balance,symbol,lev):
         msg = "MARGIN-CALL!!!\nยอดเงินต่ำกว่าที่กำหนดไว้  : " + str(min_balance)
     notify.send(msg)
     return
-    
+
+#OpenShort SELL
 def OpenShort(df,balance,symbol,lev):
     print('Entry Long')
     amount = float(buysize(df,balance,symbol))
@@ -260,12 +270,12 @@ def OpenShort(df,balance,symbol,lev):
     params = {
     'stopLoss': {
         'type': 'market', # or 'limit'
-        'stopLossPrice': df['highest'][len(df.index)-1],
+        'stopLossPrice': str(df['Highest'][len(df.index)-1]),
     },
     'takeProfit': {
         'type': 'market',
-        'takeProfitPrice': RRTP(df,symbol,False),
-        'quantity': (amount*int(TPPer)/100),
+        'takeProfitPrice': str(RRTP(df,symbol,False)),
+        'quantity': str(amount*int(TPPer)/100),
     },
     'positionSide': Sside 
     }
@@ -281,11 +291,12 @@ def OpenShort(df,balance,symbol,lev):
     notify.send(msg)
     return
 
+#CloseLong Sell
 def CloseLong(df,balance,symbol,status):
     print('Close Long')
     amount = float(status["positionAmt"][len(status.index) -1])
     upnl = status["unrealizedProfit"][len(status.index) -1]
-    bid = exchange.fetchBidsAsks([symbol])[symbol]['info']['bidPrice']
+    bid = exchange.fetchBidsAsks([symboli])[symboli]['info']['bidPrice']
     params = {
     'positionSide': Lside
     }
@@ -295,20 +306,22 @@ def CloseLong(df,balance,symbol,status):
     msg ="BINANCE:\n" + "BOT         : " + BOT_NAME + "\nCoin        : " + symbol + "\nStatus      : " + "CloseLong[SELL]" + "\nAmount    : " + str(amount) +"("+str(round((amount*bid),2))+" USDT)" + "\nPrice        :" + str(bid) + " USDT" + "\nRealized P/L: " + str(round(upnl,2)) + " USDT"  +"\nBalance   :" + str(round(total,2)) + " USDT"
     notify.send(msg)
     return
-    
+ 
+#CloseShort Buy 
 def CloseShort(df,balance,symbol,status):
     print('Close Short')
     amount = abs(float(status["positionAmt"][len(status.index) -1]))
     upnl = status["unrealizedProfit"][len(status.index) -1]
-    ask = exchange.fetchBidsAsks([symbol])[symbol]['info']['askPrice']
+    ask = exchange.fetchBidsAsks([symboli])[symboli]['info']['askPrice']
     params = {
     'positionSide': Sside
     }
     order = exchange.create_order(symbol,'market','buy',amount,params)
     time.sleep(1)
     total = balance['total']['USDT']
-    msg ="BINANCE:\n" + "BOT         : " + BOT_NAME + "\nCoin        : " + symbol + "\nStatus      : " + "CloseShort[BUY]" + "\nAmount    : " + str(amount) +"("+str(round((qty_close*ask),2))+" USDT)" + "\nPrice        :" + str(ask) + " USDT" + "\nRealized P/L: " + str(round(upnl,2)) + " USDT"  +"\nBalance   :" + str(round(total,2)) + " USDT"
+    msg ="BINANCE:\n" + "BOT         : " + BOT_NAME + "\nCoin        : " + symbol + "\nStatus      : " + "CloseShort[BUY]" + "\nAmount    : " + str(amount) +"("+str(round((qty_Close*ask),2))+" USDT)" + "\nPrice        :" + str(ask) + " USDT" + "\nRealized P/L: " + str(round(upnl,2)) + " USDT"  +"\nBalance   :" + str(round(total,2)) + " USDT"
     notify.send(msg)
+
     return
 
 #clearconsol
@@ -321,12 +334,13 @@ def clearconsol():
     else:
         os.system('cls') 
 
+#Check position after fetch DATA
 def check_buy_sell_signals(df,symbol,status,balance,lev):
     longPozisyonda = False
     shortPozisyonda = False
     pozisyondami = False
+    print(f"Fetching new bars for {dt.now().isoformat()}")
     print(df.tail(5))
-    print("checking for buy and sell signals")
     last = len(df.index) -1
     previous = last - 1
         # NO Position
@@ -344,8 +358,8 @@ def check_buy_sell_signals(df,symbol,status,balance,lev):
     if pozisyondami and float(status["positionAmt"][len(status.index) -1]) < 0:
         shortPozisyonda = True
         longPozisyonda = False
-    
-    if not df['trend'][previous] and df['trend'][last]:
+    print("checking for buy and sell signals")
+    if df['buy'][last]:
         print("changed to Bullish, buy")
         if shortPozisyonda :
             CloseShort(df,balance,symbol,status)
@@ -354,7 +368,7 @@ def check_buy_sell_signals(df,symbol,status,balance,lev):
             longPozisyonda = True
         else:
             print("already in position, nothing to do")
-    if df['trend'][previous] and not df['trend'][last]:
+    if df['sell'][last]:
         print("changed to Bearish, Sell")
         if longPozisyonda :
             CloseLong(df,balance,symbol,status)
@@ -364,11 +378,8 @@ def check_buy_sell_signals(df,symbol,status,balance,lev):
         else:
             print("already in position, nothing to do")
     
-
+#Run everthing here then sent data to checking 
 def run_bot():
-    kesisim = False
-    amount = 0
-    ROE = 0
     balance = exchange.fetch_balance()
     free_balance = exchange.fetch_free_balance()      
     exchange.precisionMode = ccxt.DECIMAL_PLACES
@@ -382,19 +393,18 @@ def run_bot():
         position_bilgi = pd.DataFrame(current_positions, columns=["symbol", "entryPrice","positionSide", "unrealizedProfit", "positionAmt", "initialMargin" ,"isolatedWallet"])
         exchange.load_markets()
         market = exchange.markets[symboli]
-        print(f"Fetching new bars for {dt.now().isoformat()}")
         bars = exchange.fetch_ohlcv(symboli, timeframe=TF, since = None, limit = 500)
-        df = pd.DataFrame(bars[:-1], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+        df = pd.DataFrame(bars[:-1], columns=['timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
         df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-        df['ema'] = ta.ema(df['close'],int(EMA_FAST))
-        df['subhag'] = ta.ema(ta.linreg(df['close'],int(LINEAR),0),int(SMOOTH))
+        df['ema'] = ta.ema(df['Close'],int(EMA_FAST))
+        df['subhag'] = ta.ema(ta.linreg(df['Close'],int(LINEAR),0),int(SMOOTH))
         alphatrend(df)
         andean(df)
         pivot(df)
         vxma(df) 
+        check_buy_sell_signals(df,symboli,position_bilgi,balance,leveragei)
         print('checking current position on hold...')
         print(tabulate(position_bilgi, headers = 'keys', tablefmt = 'grid'))
-        check_buy_sell_signals(df,symboli,position_bilgi,balance,leveragei)
 
 
     
@@ -402,7 +412,7 @@ schedule.every(10).seconds.do(run_bot)
 
 while True:
     schedule.run_pending()
-    time.sleep(20)
+    time.sleep(10)
     
 if __name__ == '__main__':
     app.run(debug=True)
